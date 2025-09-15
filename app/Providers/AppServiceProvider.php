1788a9ed9b47cc7ee2c090fc538b259223c828b3
sayedehsan <sayedehsan222@gmail.com>
2025-09-15 17:08:28 +0600

    laravel passport auth setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+// use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-    }
+        $this->registerPolicies();
+        // if(!$this->app->routesAreCached()){
+        //     Passport::routes();
+        // }
+        Passport::tokensExpireIn(now()->addDays(10));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+    }   
 }
